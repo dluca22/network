ff5bd0199d1fd06d1cbaec188a130b34da2c8bca
dluca22 <luc.due@tim.it>
2022-10-19 00:20:16 +0200

    set pseudocode structure

--- network/models.py
+++ network/models.py
@@ -3,4 +3,64 @@ from django.db import models
 
 
 class User(AbstractUser):
+    # male/female?
+    # avatar/image
+    # interests?
+    # follower
+    # following
+
+    # email private??
+    pass
+
+class Post(models.Model):
+    # model for posts by user, each post is by a User_FK, has text, has Likes_m2m, has comments_FK, has History of text_FK
+        #show list of people liking?
+    # user FK
+    # text
+    # likes FK
+    # comments FK
+    # history FK
+
+    # while editing have a save btn but also a cancel edit button
+
+    # category??
+    # picture/image??
+    pass
+
+class Category(models.Model):
+    # model for set of categories a post might fall under??
+    # maybe linked to user interests?
+    pass
+
+class Follow(models.Model):
+    # table (m2m?) linking an user to another, an user cannot follow self, but can follow others
+    # follower_id
+    # followee_id
+    pass
+
+class Comment(models.Model):
+    # class for comments to post
+    #comment_text
+    #post_ID FK
+    #user_id FK
+    pass
+
+
+class Like(models.Model):
+    # model for likes to posts (m2m field?)
+    # post_id/comment_id?
+    # user_id
+    pass
+
+
+class History(models.Model):
+    # model for history of edits to Post text and Comment text
+    # old_text
+    # post/comment_id (would id conflic?? maybe 2 models for CommentHistory and PostHistory)
+    pass
+
+class Interests(models.Model):
+    # model for various categories of Interest that people might follow
+    # has preset set of keywords?
+    # is it useful if no algorithm in place?
     pass
--- network/views.py
+++ network/views.py
@@ -8,6 +8,10 @@ from .models import User
 
 
 def index(request):
+    """
+    main page is homepage where all posts are shown by all users, no restriction
+    implement as a card the structure with include, that has body, like button, n likes, username, comments, and timestamp as xyz_time_ago, and edit btn, and history show
+    """
     return render(request, "network/index.html")
 
 
@@ -61,3 +65,54 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "network/register.html")
+
+
+def post():
+    """function to register post by user
+        GET request shows the format of the modelForm
+        POST registers the post after processing
+
+        """
+    pass
+
+def profile_page():
+    """
+    user's profile page, both own or another's
+    if own, hide follow btn, show dashboard btn
+    show n people following
+    show n followers
+    show all posts descending order
+    """
+    pass
+
+def user_dashboard():
+    """
+    from userpage get access to the dasboard view, where user can change password, delete account, change profile picture, change username, change email
+    """
+    pass
+
+def comment():
+    """
+    comment to one post
+    """
+    pass
+
+def edit():
+    """
+    edit to a post or a comment, might split this into 2 or give some parameters for context to execute different functions, or might do everything via Js
+    """
+    pass
+
+def like():
+    """
+    allows an user to like a post or comment
+    """
+    pass
+
+
+def follow():
+    """
+    allows user to follow another
+    don't know if via django or Js yet
+    """
+    pass
